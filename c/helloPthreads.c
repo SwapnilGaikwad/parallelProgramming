7e53a62d567d8f6255c25098879c8b302e2b0076
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-01-25 22:38:25 +0000

    Add deterministic order of printing to C pthread code

--- c/helloPthreads.c
+++ c/helloPthreads.c
@@ -2,11 +2,18 @@
 #include<stdlib.h>
 #include<pthread.h>
 
-#define N 4
+#include<unistd.h>
+
+#define THREAD_COUNT 4
+#define TEN_MILLISEC 10000
 
 void * printHello(void *id) {
 
     long threadId = (long) id;
+
+    // Sleeping for threadId milliseconds
+    // To ensure deterministic order of printing the output
+    usleep(TEN_MILLISEC * threadId);
 
     printf("Hello from thread %ld \n", threadId);
     pthread_exit(NULL);
@@ -14,10 +21,10 @@ void * printHello(void *id) {
 
 int main(){
 
-    pthread_t thread[N];
+    pthread_t thread[THREAD_COUNT];
      long i, isErr;
 
-    for(i=0; i<N; i++){
+    for(i = 0; i < THREAD_COUNT; i++){
         isErr = pthread_create(&thread[i], NULL, printHello, (void *) i);
         if(isErr) {
             printf("Error while creating thread %ld - Error code %ld\n", i, isErr);
