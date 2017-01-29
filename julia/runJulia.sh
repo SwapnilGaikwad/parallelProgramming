05b224ef61336f85b19246b4733c4f076641f7c1
Swapnil Gaikwad <gaikwad.swapnil@gmail.com>
2017-01-29 22:15:07 +0000

    Update julia execution script

--- julia/runJulia.sh
+++ julia/runJulia.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-julia -p 4 helloParallel.jl
+juliaExe=$(which julia)
+if [ $? -eq 0 ]; then
+ $juliaExe -p 4 helloParallel.jl
+else
+  echo "No executable found for running julia code"
+fi
